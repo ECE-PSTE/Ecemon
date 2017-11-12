8905daa61fb7025fce91ca881af8430c18c20aa1
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2017-11-12 13:15:07 +0100

    Temp commite for update from master
    ///////

--- code/models/include/Saver.h
+++ code/models/include/Saver.h
@@ -1,46 +1,29 @@
-#ifndef DEF_SAUVEGARDE
-#define DEF_SAUVEGARDE
-
-#include "EnergyCard.h"
-#include "Profil.h"
+#ifndef SAVER_H
+#define SAVER_H
 
 #include <iostream>
+
+#include "../../entities/include/Profil.h"
+#include "../../entities/include/Card.h"
+
 #include <fstream>
 #include <vector>
 #include <map>
 
-class Saver{
+class Saver
+{
     private:
         std::vector<Profil*> m_dataProfil;
         //int id card / pointeur of card
         std::map<int, Card*> m_dataCard;
 
-        //Save a card in the fill pass, the card is in a map with his Id and pointeur of card
-        void saveCardOnFil(const std::pair<const int, Card*> pMap, std::ofstream filCard);
-
     public:
+        /** Default constructor */
         Saver();
+        /** Default destructor */
         ~Saver();
 
-        //GETTER : pointeur of m_dataProfil
-        std::vector<Profil*>* getpDataProfil(){return &m_dataProfil;}
+        void testaff();
 
-        //GETTER : pointeur of m_dataCard
-        std::map<int, Card*>* getpDataCard(){return &m_dataCard;}
-
-        //GETTER : m_dataCard
-        std::map<int, Card*> getDataCard(){return m_dataCard;}
-
-        //Function who load all data on .txt to our game's Profil of players and Card
-        void loadSauvegarde();
-
-        //Function who right on .txt every infomrations about card and profil for tne next use of application
-        void saveSauvegarde();
-
-        //Function who will save in SauvegardeData/CardData.txt all data about our card
-        void saveDataCard();
-
-    };
-
-
-#endif
+};
+#endif // SAVER_H
