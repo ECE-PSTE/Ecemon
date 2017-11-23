af17b0909682fb11755afbe98e0b754d8ccbe5ab
Omar Aflak <aflakomar@hotmail.fr>
2017-11-23 23:47:47 +0100

    GCreatureCard.... done.

--- code/graphics/include/GCreatureCard.h
+++ code/graphics/include/GCreatureCard.h
@@ -8,14 +8,14 @@ class GCreatureCard : public GCard {
     private:
         sf::Texture m_texture;
         sf::Sprite m_sprite;
+        sf::Text m_lifeText;
         std::string m_imagePath;
 
     public:
         GCreatureCard();
         GCreatureCard(const Card *card, sf::RenderWindow *window);
 
-        std::string getImagePath() const;
-        void setImagePath(std::string imagePath, float scaleX, float scaleY);
+        void setImage(std::string imagePath, float scaleX, float scaleY);
 
         void update();
         void draw();
--- code/graphics/src/GCreatureCard.cpp
+++ code/graphics/src/GCreatureCard.cpp
@@ -19,13 +19,16 @@ GCreatureCard::GCreatureCard(const Card *card, sf::RenderWindow *window){
     m_descriptionText.setString(card->getDescription());
     sf::FloatRect descRect = m_descriptionText.getLocalBounds();
     m_descriptionText.setOrigin(descRect.left + descRect.width/2.0f, descRect.top  + descRect.height/2.0f);
+
+    m_lifeText.setFont(m_font);
+    m_lifeText.setCharacterSize(24);
+    m_lifeText.setColor(sf::Color::Red);
+    m_lifeText.setString(std::to_string(((const CreatureCard*)card)->getLife()));
+    sf::FloatRect lifeRect = m_lifeText.getLocalBounds();
+    m_lifeText.setOrigin(lifeRect.left + lifeRect.width/2.0f, lifeRect.top  + lifeRect.height/2.0f);
 }
 
-std::string GCreatureCard::getImagePath() const{
-    return m_imagePath;
-}
-
-void GCreatureCard::setImagePath(std::string imagePath, float scaleX, float scaleY){
+void GCreatureCard::setImage(std::string imagePath, float scaleX, float scaleY){
     m_imagePath = imagePath;
     assert(m_texture.loadFromFile(imagePath));
     m_texture.setSmooth(true);
@@ -37,6 +40,7 @@ void GCreatureCard::setImagePath(std::string imagePath, float scaleX, float scal
 void GCreatureCard::update(){
     m_cardShape.setPosition(m_position.x, m_position.y);
     m_nameText.setPosition(m_position.x, m_position.y-m_size.y/2 + 30);
+    m_lifeText.setPosition(m_position.x, m_position.y-m_size.y/2 + 60);
     m_descriptionText.setPosition(m_position.x, m_position.y+m_size.y/2-30);
     m_sprite.setPosition(m_position.x, m_position.y);
 }
@@ -45,4 +49,5 @@ void GCreatureCard::draw(){
     update();
     GCard::draw();
     m_window->draw(m_sprite);
+    m_window->draw(m_lifeText);
 }
