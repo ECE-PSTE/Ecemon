aeaf57bdffa2422e5714ca0e9933e978e3cf92bc
Omar Aflak <aflakomar@hotmail.fr>
2017-11-25 15:13:45 +0100

    completed GPowerCard + added images

--- code/graphics/src/GPowerCard.cpp
+++ code/graphics/src/GPowerCard.cpp
@@ -5,6 +5,13 @@ GPowerCard::GPowerCard(){
 
 GPowerCard::GPowerCard(const Card* card, sf::RenderWindow* window){
     GCard::init(card, window);
+
+    m_cardShape.setFillColor(sf::Color(150, 50, 250));
+    m_cardShape.setOutlineColor(sf::Color(0, 255, 10));
+    m_cardShape.setOutlineThickness(3);
+
+    m_nameText.setColor(sf::Color(0, 255, 0));
+    m_descriptionText.setColor(sf::Color(0, 255, 0));
 }
 
 void GPowerCard::update(){
