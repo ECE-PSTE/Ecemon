db65c2e95baae92422012da79727f414cefe56ec
Omar Aflak <aflakomar@hotmail.fr>
2017-11-26 13:15:37 +0100

    gridcard auto scale + position from center

--- code/graphics/include/GCardGrid.h
+++ code/graphics/include/GCardGrid.h
@@ -2,18 +2,24 @@
 #define DEF_GCARDGRID
 
 #include <vector>
+#include <cmath>
 #include "GCard.h"
 
 class GCardGrid {
     private:
-        sf::Vector2u m_gridSize;
         std::vector<GCard*> m_gcards;
+        sf::Vector2f m_position;
+        int m_maxCardsPerLine;
+        int m_spaceBetweenCards;
 
     public:
-        GCardGrid(sf::Vector2u m_gridSize);
+        GCardGrid(int maxCardsPerLine);
+
+        sf::Vector2f getPosition() const;
 
         void addGCard(GCard* gcard);
         void addGCards(std::vector<GCard*> gcards);
+        void setPosition(sf::Vector2f position);
 
         void update();
         void draw();
--- code/graphics/src/GCardGrid.cpp
+++ code/graphics/src/GCardGrid.cpp
@@ -1,7 +1,12 @@
 #include "../include/GCardGrid.h"
 
-GCardGrid::GCardGrid(sf::Vector2u gridSize){
-    m_gridSize = gridSize;
+GCardGrid::GCardGrid(int maxCardsPerLine){
+    m_maxCardsPerLine = maxCardsPerLine;
+    m_spaceBetweenCards = 5;
+}
+
+sf::Vector2f GCardGrid::getPosition() const{
+    return m_position;
 }
 
 void GCardGrid::addGCard(GCard* gcard){
@@ -12,19 +17,35 @@ void GCardGrid::addGCards(std::vector<GCard*> gcards){
     m_gcards.insert(m_gcards.end(), gcards.begin(), gcards.end());
 }
 
-void GCardGrid::update(){
-    int cardWidth = m_gcards[0]->getSize().x;
-    int cardHeight = m_gcards[0]->getSize().y;
-    int space = 5;
-    int nbCardPerLine = m_gridSize.x/(cardWidth+space);
+void GCardGrid::setPosition(sf::Vector2f position){
+    m_position = position;
+}
 
-    int x=0,y=0;
-    for(int i=0 ; i<m_gcards.size() ; i++){
-        m_gcards[i]->setPosition(sf::Vector2f(cardWidth/2+x*cardWidth+(x+1)*space, cardHeight/2+y*cardHeight+(y+1)*space));
-        x++;
-        if(i+1==nbCardPerLine){
-            y++;
-            x=0;
+void GCardGrid::update(){
+    if(m_gcards.size()>0){
+        int cardWidth = m_gcards[0]->getSize().x;
+        int cardHeight = m_gcards[0]->getSize().y;
+        int nbOfLines = ceil((float)m_gcards.size()/m_maxCardsPerLine);
+        int gridWidth = m_maxCardsPerLine*cardWidth+(m_maxCardsPerLine-1)*m_spaceBetweenCards;
+        int gridHeight = nbOfLines*cardHeight+(nbOfLines-1)*m_spaceBetweenCards;
+
+        m_position.x -= gridWidth/2;
+        m_position.y -= gridHeight/2;
+
+        int x=0,y=0;
+        for(int i=0 ; i<m_gcards.size() ; i++){
+            m_gcards[i]->setPosition(
+                sf::Vector2f(
+                    m_position.x + cardWidth/2 + x*cardWidth + (x+1)*m_spaceBetweenCards,
+                    m_position.y + cardHeight/2 + y*cardHeight + (y+1)*m_spaceBetweenCards
+                )
+            );
+
+            x++;
+            if(i+1==m_maxCardsPerLine){
+                y++;
+                x=0;
+            }
         }
     }
 }
