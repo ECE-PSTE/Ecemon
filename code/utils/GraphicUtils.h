070c4e3d711d347b5505889239c9d43b4f849fbb
Omar Aflak <aflakomar@hotmail.fr>
2017-12-01 19:27:10 +0100

    removed useless include

---
+++ code/utils/GraphicUtils.h
@@ -0,0 +1,35 @@
+#ifndef DEF_GAME
+#define DEF_GAME
+
+#include <map>
+#include <fstream>
+
+#include "Utils.h"
+
+namespace GameUtils {
+    inline const std::map<int, std::string> loadCardImages(){
+        std::map<int, std::string> images;
+        std::ifstream file("CardImages"));
+        if(file){
+            std::string line;
+            int nbCards;
+
+            getline(file, line);
+            nbCards = Utils::toInt(line);
+
+            int id;
+            for(int i=0 ; i<nbCards ; i++){
+                getline(file, line);
+                id = Utils::toInt(line);
+                getline(file, line);
+                images[id] = line;
+            }
+        }
+        file.close();
+        return images;
+    }
+
+    const std::map<int, std::string> CardImages = loadCardImages();
+};
+
+#endif
