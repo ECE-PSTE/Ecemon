5164c8a1de702438d39dc01a635083a92f1c7a3b
Omar Aflak <aflakomar@hotmail.fr>
2017-12-01 19:30:38 +0100

    added images filename constants

--- code/entities/include/Constants.h
+++ code/entities/include/Constants.h
@@ -17,6 +17,9 @@ namespace Constants {
     inline std::string DefaultCardsFilename(){
         return "GameCards";
     }
+    inline std::string DefaultCardImagesFilename(){
+        return "GameCardImages";
+    }
     inline std::string DefaultProfilesFilename(){
         return "GameProfiles";
     }
